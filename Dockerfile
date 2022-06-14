aa52867eb49120b21b350b180719328a6b3eef79
Maximilian Moehl <maximilian.moehl@gmail.com>
2022-06-14 09:54:33 +0200

    improve certbot usage

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM docker.io/certbot/dns-route53:latest
 
-RUN apk add --update coreutils curl jq && \
+RUN apk add --update coreutils curl jq bash && \
     curl -Lo /usr/local/bin/kubectl "https://dl.k8s.io/release/v1.24.0/bin/linux/amd64/kubectl" && \
     chmod +x /usr/local/bin/kubectl
 
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env ash
+#!/usr/bin/env bash
 
 set -e
 
@@ -10,13 +10,35 @@ if [[ -z "${EMAIL}" ]]; then
   log "EMAIL is empty"
   exit 1
 fi
-if [[ -z "${DOMAIN}" ]]; then
-  log "DOMAIN is empty"
-  exit 1
-fi
 if [[ -z "${NAMESPACE}" ]]; then
   log "NAMESPACE is empty"
   exit 1
+fi
+if [[ -z "${CERT_NAME}" ]]; then
+  log "CERT_NAME is empty"
+  exit 1
+fi
+
+log "requesting certificate for domains: (${*})"
+
+args=()
+for domain in "${@}"; do
+  args+=('-d' "${domain}")
+done
+
+if [[ "${DRY_RUN:+set}" == "set" ]]; then
+  log "dry-run, would execute:
+certbot
+    certonly
+    -n
+    --agree-tos
+    -m ${EMAIL}
+    --dns-route53
+    --dns-route53-propagation-seconds 30
+    --cert-name ${CERT_NAME}
+    ${args[*]}"
+  log "exiting"
+  exit 0
 fi
 
 log "requesting certificate"
@@ -27,12 +49,8 @@ certbot \
   -m "${EMAIL}" \
   --dns-route53 \
   --dns-route53-propagation-seconds 30 \
-  -d "${DOMAIN}"
-
-# certbot removes '*.' from the beginning of the domain when writing the certs
-if [[ "${DOMAIN:0:2}" == '*.' ]]; then
-  DOMAIN="${DOMAIN:2}"
-fi
+  --cert-name "${CERT_NAME}" \
+  "${args[@]}"
 
 log "writing certificate to kubernetes secret ${NAMESPACE}/${SECRET_NAME}"
 JSON_PATCH="$(printf '{
@@ -40,6 +58,6 @@ JSON_PATCH="$(printf '{
     "tls.crt": "%s",
     "tls.key": "%s"
   }
-}' "$(base64 -w0 "/etc/letsencrypt/live/${DOMAIN}/fullchain.pem")" "$(base64 -w0 "/etc/letsencrypt/live/${DOMAIN}/privkey.pem")")"
+}' "$(base64 -w0 "/etc/letsencrypt/live/${CERT_NAME}/fullchain.pem")" "$(base64 -w0 "/etc/letsencrypt/live/${CERT_NAME}/privkey.pem")")"
 JSON_PATCH="$(echo "${JSON_PATCH}" | jq -c '.')"
 kubectl -n "${NAMESPACE}" patch secret "${SECRET_NAME}" --patch "${JSON_PATCH}"
